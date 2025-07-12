02e671bacac96d356d71eabf8c73e1553ea8ec63
ShreyRawat-bit <shreyrawat59@gmail.com>
2025-07-12 11:47:30 +0000

    add: 02_dataTypes.js and update 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144533
+let accountPassword="23222"
+var accountState="Haryana"
+accountCity="Dehradun"
+
+accountPassword="23222"
+accountState="Haryana"
+accountCity="Dehradun"
+console.table([accountId,accountState,accountPassword,accountCity])
