daea9bbfe842b2c13018dc1ca1170a92f368d6a2
ShreyRawat-bit <shreyrawat59@gmail.com>
2025-08-06 06:54:23 +0000

    Update functions.js in 02_basics

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -1,27 +1,44 @@
-function saymyname(){
-    console.log("hi")
-    console.log("my name is")
-    console.log("Shrey")
-}
-saymyname()
-function addtwonumbers(number1,number2){
-    console.log(number1+number2)
-}
-const ans=addtwonumbers(3,9)
-console.log(ans)
-saymyname()
-function addtwonumbers2(number1,number2){
-    console.log(number1+number2)
-    return (number1+number2)
-}
-const ans2=addtwonumbers(3,9)
-console.log(ans2)
-function logger(username){
-    if(username===undefined){
-        console.log("Enter values bruh")
-        return
-    }
-    return `${username} just logger in`
-}
-// console.log(logger("Shrey"))
-console.log(logger())
+// function saymyname(){
+//     console.log("hi")
+//     console.log("my name is")
+//     console.log("Shrey")
+// }
+// saymyname()
+// function addtwonumbers(number1,number2){
+//     console.log(number1+number2)
+// }
+// const ans=addtwonumbers(3,9)
+// console.log(ans)
+// saymyname()
+// function addtwonumbers2(number1,number2){
+//     console.log(number1+number2)
+//     return (number1+number2)
+// }
+// const ans2=addtwonumbers(3,9)
+// console.log(ans2)
+// function logger(username){
+//     if(username===undefined){
+//         console.log("Enter values bruh")
+//         return
+//     }
+//     return `${username} just logger in`
+// }
+// // console.log(logger("Shrey"))
+// console.log(logger())
+// const myobj={
+//     name:"shrey",
+//     age:20,
+//     class:"s"
+// }
+// for (const num in myobj){
+//     console.log(num)
+// }
+// if(Object.keys(myobj).length!=0){
+//     console.log("not empty")}
+// const myarr=[1,2,3,4,5]
+// myarr.forEach((item)=>{
+//     console.log(`so the item here is ${item}`)
+// })
+const mynum=["hi","how ","are","you"]
+const newnums=mynum.filter((item)=>item>"m")
+console.log(newnums)
